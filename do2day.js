eecf0e72af6737a18a5e2e7cf25f33a0663e3420
Sarah Capecci <shlcapecci@gmail.com>
2014-06-27 10:01:59 -0400

    first view working properly

--- do2day.js
+++ do2day.js
@@ -3,7 +3,8 @@ var app = {}; // create namespace for our app
 app.Todo = Backbone.Model.extend({
   defaults: {
     title: '',
-    hours: ''
+    hoursExpected: '',
+    hoursSpent: ''
   }
 });
 
@@ -12,31 +13,12 @@ app.TodoList = Backbone.Collection.extend({
       localStorage: new Store("backbone-todo")
 });
 
-    // instance of the Collection
-    app.todoList = new app.TodoList();
-
-
-// var AppView = Backbone.View.extend({
-//       // el - stands for element. Every view has a element associate in with HTML 
-//       //      content will be rendered.
-//       el: '#container',
-//       // It's the first function called when this view it's instantiated.
-//       initialize: function(){
-//         this.render();
-//       },
-//       // $el - it's a cached jQuery object (el), in which you can use jQuery functions 
-//       //       to push content. Like the Hello World in this case.
-//       render: function(){
-//         this.$el.html("Hello World");
-//       }
-// });
-
-
 
 app.AppView = Backbone.View.extend({
       el: '#expected',
       initialize: function () {
         this.input = this.$("#new-todo");
+        this.range = this.$('#expected-time');
         // when new elements are added to the collection render then with addOne
         app.todoList.on('add', this.addOne, this);
         app.todoList.on('reset', this.addAll, this);
@@ -48,6 +30,7 @@ app.AppView = Backbone.View.extend({
       createTask: function(){
         app.todoList.create(this.newAttributes());
         this.input.val(''); // clean input box
+        this.range.val('1'); // reset range to 1
       },
       addOne: function(todo){
         var view = new app.TodoView({model: todo});
@@ -56,14 +39,15 @@ app.AppView = Backbone.View.extend({
       },
       addAll: function(){
         this.$('#todo-list').html('');
+        this.$('#todo-list-copy').html('');
          // clean the todo list
         app.todoList.each(this.addOne, this);
       },
       newAttributes: function(){
         return {
           title: this.input.val(),
-          hours: this.$('input[type=range]').val(),
-          completed: false
+          hoursExpected: this.range.val()
+          
         }
       }
 });
@@ -84,20 +68,7 @@ app.AppView2 = Backbone.View.extend({
 
       },
 
-      addOne: function(todo){
-        var view = new app.TodoView({model: todo});
-        $('#todo-list').append(view.render().el);
-      },
-      addAll: function(){
-        this.$('#todo-list').html(''); // clean the todo list
-        app.todoList.each(this.addOne, this);
-      },
-      newAttributes: function(){
-        return {
-          hours: this.$('input[type=range]').val(),
-          completed: false
-        }
-      }
+
 });
 
 
@@ -108,7 +79,7 @@ app.TodoView = Backbone.View.extend({
       template: _.template($('#item-template').html()),
       render: function(){
         this.$el.html(this.template(this.model.toJSON()));
-        this.input = this.$('.edit');
+        this.input = this.$('#new-todo');
         return this; // enable chained calls
       },
       initialize: function(){
@@ -123,14 +94,15 @@ app.TodoView = Backbone.View.extend({
       edit: function(){
         this.$el.addClass('nowEditing');
       },
+      
       close: function(){
-        var value = this.$('input[type=text]').val().trim();
+        var value = this.input.val().trim();
         if(value) {
           this.model.save({title: value});
         };
-        var rangeValue = this.$('input[type=range]').val();
+        var rangeValue = this.input.val();
         if(rangeValue) {
-          this.model.save({hours: rangeValue});
+          this.model.save({hoursExpected: rangeValue});
         }
 
         this.$el.removeClass('nowEditing');
@@ -145,34 +117,29 @@ app.TodoView = Backbone.View.extend({
 
 app.TodoView2 = Backbone.View.extend({
       tagName: 'li',
+
       template: _.template($('#item-template-2').html()),
+
       render: function(){
         this.$el.html(this.template(this.model.toJSON()));
-        
         return this; // enable chained calls
       },
+
       initialize: function(){
         this.model.on('change', this.render, this);
       },
+
       events: {
-       
-      },
-      edit: function(){
-        this.$el.addClass('nowEditing');
+        'click .saveTime': 'updateValue'
       },
       close: function(){
-
-        var rangeValue = this.$('input[type=range]').val();
-        if(rangeValue) {
-          this.model.save({hours: rangeValue});
+        var rangeValueReal = this.$('.real-time').val();
+        if(rangeValueReal) {
+          this.model.save({hoursSpent: rangeValueReal});
         }
-        this.$el.removeClass('nowEditing');
       },
       updateValue: function(){
           this.close(); 
-      },
-      destroy: function(){
-        this.model.destroy();
       }
 });
 
@@ -181,11 +148,16 @@ app.Router = Backbone.Router.extend({
       routes: {
         '*filter' : 'setFilter'
       },
-      setFilter: function(params) {
+      setFilter: function() {
         app.todoList.trigger('reset');
       }
 });
 
+
+
+
+    // instance of the Collection
+    app.todoList = new app.TodoList();
 app.router = new app.Router();
 Backbone.history.start(); 
 app.appView = new app.AppView();
