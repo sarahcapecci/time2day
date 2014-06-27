3e0d800dbbd492113fd8a509d5d4eaa4e10d8e3d
Sarah Capecci <shlcapecci@gmail.com>
2014-06-26 23:44:58 -0400

    made some progress

--- do2day.js
+++ do2day.js
@@ -52,9 +52,11 @@ app.AppView = Backbone.View.extend({
       addOne: function(todo){
         var view = new app.TodoView({model: todo});
         $('#todo-list').append(view.render().el);
+        $('#todo-list-copy').append(view.render().el);
       },
       addAll: function(){
-        this.$('#todo-list').html(''); // clean the todo list
+        this.$('#todo-list').html('');
+         // clean the todo list
         app.todoList.each(this.addOne, this);
       },
       newAttributes: function(){
@@ -70,19 +72,18 @@ app.AppView = Backbone.View.extend({
 app.AppView2 = Backbone.View.extend({
       el: '#real',
       initialize: function () {
-        this.input = this.$("#new-todo");
         // when new elements are added to the collection render then with addOne
         app.todoList.on('add', this.addOne, this);
         app.todoList.on('reset', this.addAll, this);
         app.todoList.fetch(); // Loads list from local storage
       },
       events: {
-        'click .saveTask': 'createTodoOnEnter'
+        'click .saveTime': 'saveTime'
       },
-      createTodoOnEnter: function(){
-        app.todoList.create(this.newAttributes());
-        this.input.val(''); // clean input box
+      saveTime: function(){
+
       },
+
       addOne: function(todo){
         var view = new app.TodoView({model: todo});
         $('#todo-list').append(view.render().el);
@@ -98,7 +99,6 @@ app.AppView2 = Backbone.View.extend({
         }
       }
 });
-
 
 
 
@@ -143,6 +143,39 @@ app.TodoView = Backbone.View.extend({
       }
 });
 
+app.TodoView2 = Backbone.View.extend({
+      tagName: 'li',
+      template: _.template($('#item-template-2').html()),
+      render: function(){
+        this.$el.html(this.template(this.model.toJSON()));
+        
+        return this; // enable chained calls
+      },
+      initialize: function(){
+        this.model.on('change', this.render, this);
+      },
+      events: {
+       
+      },
+      edit: function(){
+        this.$el.addClass('nowEditing');
+      },
+      close: function(){
+
+        var rangeValue = this.$('input[type=range]').val();
+        if(rangeValue) {
+          this.model.save({hours: rangeValue});
+        }
+        this.$el.removeClass('nowEditing');
+      },
+      updateValue: function(){
+          this.close(); 
+      },
+      destroy: function(){
+        this.model.destroy();
+      }
+});
+
 
 app.Router = Backbone.Router.extend({
       routes: {
@@ -156,5 +189,4 @@ app.Router = Backbone.Router.extend({
 app.router = new app.Router();
 Backbone.history.start(); 
 app.appView = new app.AppView();
-
-app.appView2 = new app.AppView();
+app.appView2 = new app.AppView2();
