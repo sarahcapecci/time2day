6b767b6f0cca22f08903d2261d0be23bbc5fe3de
Sarah Capecci <shlcapecci@gmail.com>
2014-06-26 20:14:15 -0400

    there's a bug, but it's fine

--- do2day.js
+++ do2day.js
@@ -3,15 +3,14 @@ var app = {}; // create namespace for our app
 app.Todo = Backbone.Model.extend({
   defaults: {
     title: '',
-    hours: '',
-    completed: false
+    hours: ''
   }
 });
 
 app.TodoList = Backbone.Collection.extend({
       model: app.Todo,
       localStorage: new Store("backbone-todo")
-    });
+});
 
     // instance of the Collection
     app.todoList = new app.TodoList();
@@ -33,32 +32,20 @@ app.TodoList = Backbone.Collection.extend({
 // });
 
 
-app.TodoView = Backbone.View.extend({
-      tagName: 'li',
-      template: _.template($('#item-template').html()),
-      render: function(){
-        this.$el.html(this.template(this.model.toJSON()));
-        return this; // enable chained calls
-      }
-});
-
 
 app.AppView = Backbone.View.extend({
       el: '#expected',
       initialize: function () {
-        this.input = this.$('#new-todo');
+        this.input = this.$("#new-todo");
         // when new elements are added to the collection render then with addOne
         app.todoList.on('add', this.addOne, this);
         app.todoList.on('reset', this.addAll, this);
         app.todoList.fetch(); // Loads list from local storage
       },
       events: {
-        'keypress #new-todo': 'createTodoOnEnter'
+        'click .saveTask': 'createTask'
       },
-      createTodoOnEnter: function(e){
-        if ( e.which !== 13 || !this.input.val().trim() ) { // ENTER_KEY = 13
-          return;
-        }
+      createTask: function(){
         app.todoList.create(this.newAttributes());
         this.input.val(''); // clean input box
       },
@@ -72,47 +59,48 @@ app.AppView = Backbone.View.extend({
       },
       newAttributes: function(){
         return {
-          title: this.input.val().trim(),
+          title: this.input.val(),
+          hours: this.$('input[type=range]').val(),
           completed: false
         }
       }
 });
 
 
-// app.AppView = Backbone.View.extend({
-//       el: '#real',
-//       initialize: function () {
-//         this.input = this.$('#new-todo');
-//         // when new elements are added to the collection render then with addOne
-//         app.todoList.on('add', this.addOne, this);
-//         app.todoList.on('reset', this.addAll, this);
-//         app.todoList.fetch(); // Loads list from local storage
-//       },
-//       events: {
-//         'keypress #new-todo': 'createTodoOnEnter'
-//       },
-//       createTodoOnEnter: function(e){
-//         if ( e.which !== 13 || !this.input.val().trim() ) { // ENTER_KEY = 13
-//           return;
-//         }
-//         app.todoList.create(this.newAttributes());
-//         this.input.val(''); // clean input box
-//       },
-//       addOne: function(todo){
-//         var view = new app.TodoView({model: todo});
-//         $('#todo-list').append(view.render().el);
-//       },
-//       addAll: function(){
-//         this.$('#todo-list').html(''); // clean the todo list
-//         app.todoList.each(this.addOne, this);
-//       },
-//       newAttributes: function(){
-//         return {
-//           title: this.input.val().trim(),
-//           completed: false
-//         }
-//       }
-// });
+app.AppView2 = Backbone.View.extend({
+      el: '#real',
+      initialize: function () {
+        this.input = this.$("#new-todo");
+        // when new elements are added to the collection render then with addOne
+        app.todoList.on('add', this.addOne, this);
+        app.todoList.on('reset', this.addAll, this);
+        app.todoList.fetch(); // Loads list from local storage
+      },
+      events: {
+        'click .saveTask': 'createTodoOnEnter'
+      },
+      createTodoOnEnter: function(){
+        app.todoList.create(this.newAttributes());
+        this.input.val(''); // clean input box
+      },
+      addOne: function(todo){
+        var view = new app.TodoView({model: todo});
+        $('#todo-list').append(view.render().el);
+      },
+      addAll: function(){
+        this.$('#todo-list').html(''); // clean the todo list
+        app.todoList.each(this.addOne, this);
+      },
+      newAttributes: function(){
+        return {
+          hours: this.$('input[type=range]').val(),
+          completed: false
+        }
+      }
+});
+
+
+
 
 
 app.TodoView = Backbone.View.extend({
@@ -128,28 +116,28 @@ app.TodoView = Backbone.View.extend({
         this.model.on('destroy', this.remove, this);
       },
       events: {
-        'dblclick label' : 'edit',
-        'keypress .edit' : 'updateOnEnter',
-        'blur .edit' : 'close',
+        'click .edit' : 'edit',
+        'click .doneEditing' : 'updateValue',
        'click .destroy': 'destroy'
       },
       edit: function(){
-        this.$el.addClass('editing');
-        this.input.focus();
+        this.$el.addClass('nowEditing');
       },
       close: function(){
-        var value = this.input.val().trim();
+        var value = this.$('input[type=text]').val().trim();
         if(value) {
           this.model.save({title: value});
+        };
+        var rangeValue = this.$('input[type=range]').val();
+        if(rangeValue) {
+          this.model.save({hours: rangeValue});
         }
-        this.$el.removeClass('editing');
-      },
-      updateOnEnter: function(e){
-        if(e.which == 13){
-          this.close();
-        }
-      },
 
+        this.$el.removeClass('nowEditing');
+      },
+      updateValue: function(){
+          this.close(); 
+      },
       destroy: function(){
         this.model.destroy();
       }
@@ -168,3 +156,5 @@ app.Router = Backbone.Router.extend({
 app.router = new app.Router();
 Backbone.history.start(); 
 app.appView = new app.AppView();
+
+app.appView2 = new app.AppView();
