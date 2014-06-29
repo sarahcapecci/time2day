4c987deb5a95926a36be2961e53c091bbf2d2145
Sarah Capecci <shlcapecci@gmail.com>
2014-06-29 11:10:30 -0400

    design changes

--- do2day.js
+++ do2day.js
@@ -215,12 +215,52 @@ var timeChart = new Chart(document.getElementById("myChart").getContext("2d")).B
   
 // });
 
-function updateTextInput(val) {
-      document.getElementById('textInput').value=val; 
-}
 
-function updateTextInput(val) {
-      document.getElementById('editedTextInput').value=val; 
-    }
+
+
+
+
+
+// DOM Ready
+$(function() {
+ var el, newPoint, newPlace, offset;
+ 
+ // Select all range inputs, watch for change
+ $("input[type='range']").change(function() {
+ 
+   // Cache this for efficiency
+   el = $(this);
+   
+   // Measure width of range input
+   width = el.width();
+   
+   // Figure out placement percentage between left and right of input
+   newPoint = (el.val() - el.attr("min")) / (el.attr("max") - el.attr("min"));
+   
+   // Janky value to get pointer to line up better
+   offset = -1.3;
+   
+   // Prevent bubble from going beyond left or right (unsupported browsers)
+   if (newPoint < 0) { newPlace = 0; }
+   else if (newPoint > 1) { newPlace = width; }
+   else { newPlace = width * newPoint + offset; offset -= newPoint; }
+   
+   // Move bubble
+   el
+     .next("output")
+     .css({
+       left: newPlace,
+       marginLeft: offset + "%"
+     })
+     .text(el.val());
+ })
+ // Fake a change to position bubble at page load
+ .trigger('change');
+});
+
+
+$('a').on("click", function(e){
+  e.preventDefault();
+});
 
 
